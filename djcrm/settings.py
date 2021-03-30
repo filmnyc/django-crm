79c852f2b4c0e632d0de17e6a7539b3a80bda851
Jonathan Faber <jon@filmnyc.com>
2021-03-30 18:20:54 -0400

    deleted secret key

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uk)815uz%ak4k!ae3+f##m$cr2ty7ra%)tbo#2t#g1%lny_h%_'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
