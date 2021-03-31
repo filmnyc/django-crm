2247e35c68b885b37163e0ea4f7d36ee068592f2
Jonathan Faber <jon@filmnyc.com>
2021-03-31 05:41:19 -0400

    update models ran admin site

--- leads/models.py
+++ leads/models.py
@@ -8,7 +8,8 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.user.email
 
 #    SOURCE_CHOICES = (
 #            ("YouTube", "YouTube"),
@@ -23,6 +24,9 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 # CASCADE: If Agent is deleted, user is deleted as well.
 # SET_DEFAULT, null=True, default=.
