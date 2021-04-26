69e944e12d429e0270aa66b02811a2c884883fb0
Jonathan Faber <jon@filmnyc.com>
2021-04-26 05:37:31 -0400

    update categories

--- leads/views.py
+++ leads/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views import generic
 from agents.mixins import OrganiserAndLoginRequiredMixin
 from .models import Lead, Agent, Category
-from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm, LeadCategoryUpdateForm
 
 
 class SignupView(generic.CreateView):
@@ -204,14 +204,6 @@ class CategoryDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "leads/category_detail.html"
     context_object_name = "category"
 
-#    def get_context_data(self, **kwargs):
-#        context = super(CategoryDetailView, self).get_context_data(**kwargs)
-#        leads = self.get_object().leads.all()
-#
-#        context.update({
-#            "leads": leads 
-#        })
-#        return context
 
     def get_queryset(self):
         user = self.request.user
@@ -250,6 +242,23 @@ class AssignAgentView(OrganiserAndLoginRequiredMixin, generic.FormView):
         return super(AssignAgentView, self).form_valid(form)
 
 
+class LeadCategoryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "leads/lead_category_update.html"
+    form_class = LeadCategoryUpdateForm
+
+    def get_queryset(self):
+        user = self.request.user
+        # initial queryset of leads for the entire organisation
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organisation=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organisation=user.agent.organisation)
+            # filter for the agent that is logged in
+            queryset = queryset.filter(agent__user=user)
+        return queryset
+
+    def get_success_url(self):
+        return reverse("leads:lead-detail", kwargs={"pk": self.get_object().id})
 # def lead_update(request, pk):
 #     lead = Lead.objects.get(id=pk)
 #     form = LeadForm()
